99b535c566bb0c7595fc6d06e6b71bac77f20252
VlAdA4 <pidoras3185@mail.ru>
2021-07-11 16:53:07 +0300

    final

--- bk_bot.py
+++ bk_bot.py
@@ -14,51 +14,67 @@ token = '5aab5f0fd363d2e904c77896ed1861377251dc1c8f401defda9bd17ddcbc3ed7bf0f3b8
 vk = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(vk)
 
+hour = 3600
+
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         if event.from_user:
-            request = event.text.upper().strip()
-            print(request, event.user_id)
-            data = p.parse_data(p.ivcht_domain)
-            write_msg(event.user_id, "Данные за " + str(datetime.now()))
-            result = 'ИВЧТ \n № | Балл | ФИО | Приоритет | Согласие на другие напр-я \n'
-            for index, item in enumerate(sorted(data, reverse=True), 1):
-                if item[1] != 'Нуржанов Айшат Бердагалиевич':
-                    result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[3] + '\n'
-                else:
-                    result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[
-                        3] + '\n' + "КОНЕЦ"
-                    break
-            write_msg(event.user_id, result)
-            data = p.parse_data(p.ifst_domain)
-            result = 'ИФСТ \n № | Балл | ФИО | Приоритет | Согласие на другие напр-я \n'
-            for index, item in enumerate(sorted(data, reverse=True), 1):
-                if item[1] != 'Нуржанов Айшат Бердагалиевич':
-                    result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[3] + '\n'
-                else:
-                    result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[
-                        3] + '\n' + "КОНЕЦ"
-                    break
-            write_msg(event.user_id, result)
-            data = p.parse_data(p.pinf_domain)
-            result = 'ПИНФ \n № | Балл | ФИО | Приоритет | Согласие на другие напр-я \n'
-            for index, item in enumerate(sorted(data, reverse=True), 1):
-                if item[1] != 'Нуржанов Айшат Бердагалиевич':
-                    result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[3] + '\n'
-                else:
-                    result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[
-                        3] + '\n' + "КОНЕЦ"
-                    break
-            write_msg(event.user_id, result)
-            data = p.parse_data(p.pinzh_domain)
-            result = 'ПИНЖ \n № | Балл | ФИО | Приоритет | Согласие на другие напр-я \n'
-            for index, item in enumerate(sorted(data, reverse=True), 1):
-                if item[1] != 'Нуржанов Айшат Бердагалиевич':
-                    result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[3] + '\n'
-                else:
-                    result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[
-                        3] + '\n' + "КОНЕЦ"
-                    break
-            write_msg(event.user_id, result)
-            print("конец сообщения")
-            time.sleep(3600)
+            try:
+                data = p.get_data()
+                write_msg(event.user_id, "Данные за " + str(datetime.now()))
+                for item in data:
+                    write_msg(event.user_id, item[0])
+                    msg = '№ | Балл | ФИО | Приоритет | Согласие на другие напр-я \n'
+                    for index, string in enumerate(item[1], 1):
+                        if string[1] != 'Нуржанов Айшат Бердагалиевич':
+                            msg += str(index) + ' ' + string[0] + ' ' + string[1] + ' ' + string[2] + ' ' + string[
+                                3] + '\n'
+                        else:
+                            msg += str(index) + ' ' + string[0] + ' ' + string[1] + ' ' + string[2] + ' ' + string[
+                                3] + '\n'
+                            break
+                    write_msg(event.user_id, msg)
+            except Exception:
+                write_msg(event.user_id, p.error_msg)
+            write_msg(event.user_id, "конец сообщения")
+            time.sleep(hour * 4)
+
+            # result = 'ИВЧТ \n № | Балл | ФИО | Приоритет | Согласие на другие напр-я \n'
+            # for index, item in enumerate(sorted(data, reverse=True), 1):
+            #     if item[1] != 'Нуржанов Айшат Бердагалиевич':
+            #         result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[3] + '\n'
+            #     else:
+            #         result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[
+            #             3] + '\n' + "КОНЕЦ"
+            #         break
+            # write_msg(event.user_id, result)
+            # data = p.parse_data(p.ifst_domain)
+            # result = 'ИФСТ \n № | Балл | ФИО | Приоритет | Согласие на другие напр-я \n'
+            # for index, item in enumerate(sorted(data, reverse=True), 1):
+            #     if item[1] != 'Нуржанов Айшат Бердагалиевич':
+            #         result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[3] + '\n'
+            #     else:
+            #         result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[
+            #             3] + '\n' + "КОНЕЦ"
+            #         break
+            # write_msg(event.user_id, result)
+            # data = p.parse_data(p.pinf_domain)
+            # result = 'ПИНФ \n № | Балл | ФИО | Приоритет | Согласие на другие напр-я \n'
+            # for index, item in enumerate(sorted(data, reverse=True), 1):
+            #     if item[1] != 'Нуржанов Айшат Бердагалиевич':
+            #         result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[3] + '\n'
+            #     else:
+            #         result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[
+            #             3] + '\n' + "КОНЕЦ"
+            #         break
+            # write_msg(event.user_id, result)
+            # data = p.parse_data(p.pinzh_domain)
+            # result = 'ПИНЖ \n № | Балл | ФИО | Приоритет | Согласие на другие напр-я \n'
+            # for index, item in enumerate(sorted(data, reverse=True), 1):
+            #     if item[1] != 'Нуржанов Айшат Бердагалиевич':
+            #         result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[3] + '\n'
+            #     else:
+            #         result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[
+            #             3] + '\n' + "КОНЕЦ"
+            #         break
+            # write_msg(event.user_id, result)
--- parse.py
+++ parse.py
@@ -5,28 +5,34 @@ ivcht_domain = 'https://abitur.sstu.ru/vpo/direction/2021/66/b/o/b'
 ifst_domain = 'https://abitur.sstu.ru/vpo/direction/2021/68/b/o/b'
 pinf_domain = 'https://abitur.sstu.ru/vpo/direction/2021/70/b/o/b'
 pinzh_domain = 'https://abitur.sstu.ru/vpo/direction/2021/71/b/o/b'
+domains = {'ИВЧТ': ivcht_domain, 'ИФСТ': ifst_domain, 'ПИНФ': pinf_domain, 'ПИНЖ': pinzh_domain}
+error_msg = "Не могу сделать запрос на сервер"
 
 
 def parse_data(url):
     page = requests.get(url)
-    soup = bs(page.text, 'html.parser')
-    table = bs(str(soup.find_all('div', attrs={'class': 'rasp-block'})[-1]), 'html.parser')
-    tbody = table.find('tbody', attrs={'class': 'text-center'})
-    data = bs(str(tbody), 'html.parser')
-    trs = data.find_all('tr')
-    lst = []
-    for tr in trs:
-        temp = bs(str(tr), 'html.parser').find_all('td')
-        lst += [[temp[2].text.strip(),
-                 temp[1].text.strip().replace('\r\n', '').split(
-                     '                                                                                                                                                        ')[
-                     0], temp[7].text.strip(), temp[8].text.strip()]]
-    return lst
+    if page.status_code == 200:
+        soup = bs(page.text, 'html.parser')
+        table = bs(str(soup.find_all('div', attrs={'class': 'rasp-block'})[-1]), 'html.parser')
+        tbody = table.find('tbody', attrs={'class': 'text-center'})
+        data = bs(str(tbody), 'html.parser')
+        trs = data.find_all('tr')
+        lst = []
+        for tr in trs:
+            temp = bs(str(tr), 'html.parser').find_all('td')
+            lst += [[temp[2].text.strip(),
+                     temp[1].text.strip().replace('\r\n', '').split(
+                         '                                                                                                                                                        ')[
+                         0], temp[7].text.strip(), temp[8].text.strip()]]
+        return lst
+    else:
+        return error_msg
 
 
-def output():
-    for index, item in enumerate(sorted(parse_data(ivcht_domain), reverse=True), 1):
-        yield index, item[0], item[1], item[2], item[3]
-
-# for i in output():
-#     print(i)
+def get_data():
+    for key in domains.keys():
+        data = parse_data(domains[key])
+        if data != error_msg:
+            yield key, sorted(data, reverse=True)
+        else:
+            return error_msg
