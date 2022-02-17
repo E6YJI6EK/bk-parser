236bbcfb6c076cce6bfbc934eaadf565d93a3b60
E6YJI6EK <47756021+E6YJI6EK@users.noreply.github.com>
2022-02-17 22:36:57 +0400

    added a couple of touches

--- bk_bot.py
+++ bk_bot.py
@@ -10,7 +10,7 @@ def write_msg(user_id, message):
     vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': get_random_id()})
 
 
-token = '5aab5f0fd363d2e904c77896ed1861377251dc1c8f401defda9bd17ddcbc3ed7bf0f3b8d18c799f1cef6e'
+token = 'Ваш токен вк апи'
 vk = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(vk)
 
@@ -26,55 +26,13 @@ for event in longpoll.listen():
                     write_msg(event.user_id, item[0])
                     msg = '№ | Балл | ФИО | Приоритет | Согласие на другие напр-я \n'
                     for index, string in enumerate(item[1], 1):
-                        if string[1] != 'Нуржанов Айшат Бердагалиевич':
-                            msg += str(index) + ' ' + string[0] + ' ' + string[1] + ' ' + string[2] + ' ' + string[
-                                3] + '\n'
+                        if string[1] != 'Ваше ФИО':
+                            msg += '{0} {1} {2} {3} {4}\n'.format(str(index), string[0], string[1], string[2], string[3])
                         else:
-                            msg += str(index) + ' ' + string[0] + ' ' + string[1] + ' ' + string[2] + ' ' + string[
-                                3] + '\n'
+                            msg += '{0} {1} {2} {3} {4}\n'.format(str(index), string[0], string[1], string[2], string[3])
                             break
                     write_msg(event.user_id, msg)
             except Exception:
                 write_msg(event.user_id, p.error_msg)
             write_msg(event.user_id, "конец сообщения")
             time.sleep(hour * 4)
-
-            # result = 'ИВЧТ \n № | Балл | ФИО | Приоритет | Согласие на другие напр-я \n'
-            # for index, item in enumerate(sorted(data, reverse=True), 1):
-            #     if item[1] != 'Нуржанов Айшат Бердагалиевич':
-            #         result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[3] + '\n'
-            #     else:
-            #         result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[
-            #             3] + '\n' + "КОНЕЦ"
-            #         break
-            # write_msg(event.user_id, result)
-            # data = p.parse_data(p.ifst_domain)
-            # result = 'ИФСТ \n № | Балл | ФИО | Приоритет | Согласие на другие напр-я \n'
-            # for index, item in enumerate(sorted(data, reverse=True), 1):
-            #     if item[1] != 'Нуржанов Айшат Бердагалиевич':
-            #         result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[3] + '\n'
-            #     else:
-            #         result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[
-            #             3] + '\n' + "КОНЕЦ"
-            #         break
-            # write_msg(event.user_id, result)
-            # data = p.parse_data(p.pinf_domain)
-            # result = 'ПИНФ \n № | Балл | ФИО | Приоритет | Согласие на другие напр-я \n'
-            # for index, item in enumerate(sorted(data, reverse=True), 1):
-            #     if item[1] != 'Нуржанов Айшат Бердагалиевич':
-            #         result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[3] + '\n'
-            #     else:
-            #         result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[
-            #             3] + '\n' + "КОНЕЦ"
-            #         break
-            # write_msg(event.user_id, result)
-            # data = p.parse_data(p.pinzh_domain)
-            # result = 'ПИНЖ \n № | Балл | ФИО | Приоритет | Согласие на другие напр-я \n'
-            # for index, item in enumerate(sorted(data, reverse=True), 1):
-            #     if item[1] != 'Нуржанов Айшат Бердагалиевич':
-            #         result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[3] + '\n'
-            #     else:
-            #         result += str(index) + ' ' + item[0] + ' ' + item[1] + ' ' + item[2] + ' ' + item[
-            #             3] + '\n' + "КОНЕЦ"
-            #         break
-            # write_msg(event.user_id, result)
